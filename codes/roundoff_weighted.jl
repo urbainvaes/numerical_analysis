99df07c2ab2806c5429a449dcfeadbc0839525d6
Urbain Vaes <urbain@vaes.uk>
2022-01-21 17:24:39 +0100

    Minor cahnges to code

--- codes/roundoff_weighted.jl
+++ codes/roundoff_weighted.jl
@@ -1,4 +1,7 @@
 N, L = 10^6, 10^9
 x = L .+ rand(N)
 
-1/(N-1) * (sum(x.^2) - N*(sum(x)/N)^2)
+average = (sum(x)/N)
+
+s1 = 1/(N-1) * (sum(x.^2) - N*(sum(x)/N)^2)
+s2 = 1/(N-1) * (sum((x .- average).^2))
